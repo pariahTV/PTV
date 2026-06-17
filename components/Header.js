55840f3dbec2d9bf62924930e6495166d95d4a26
pariahTV <pariahfm@gmail.com>
2026-06-18 00:12:08 +0100

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="ptv-header">
+      <img src="/logos/pariah-tv-full.png" alt="PariahTV Full Logo" className="ptv-header-logo" />
+      <div className="ptv-live-tag">LIVE 8.5K</div>
+    </header>
+  );
+}
