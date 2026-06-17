2a2fc8c4c7f3fab3d9172158d4daabc734527a7d
pariahTV <pariahfm@gmail.com>
2026-06-17 23:45:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1>PariahTV Viewer</h1>;
+  return (
+    <main style={{ textAlign: "center", paddingTop: "40px" }}>
+      <h1>PariahTV Viewer</h1>
+
+      <img src="/logos/pariah-tv-logo.PNG" alt="PariahTV Logo" width="180" />
+      <img src="/logos/pariah-tv-full.PNG" alt="PariahTV Full Banner" width="260" />
+      <img src="/logos/ptv-silhouette.PNG" alt="PTV Silhouette" width="140" />
+    </main>
+  );
 }
