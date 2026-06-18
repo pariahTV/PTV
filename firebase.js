b8ab8c2bb9e16387767a9e59c74820516c0108cd
pariahTV <pariahfm@gmail.com>
2026-06-18 21:10:24 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCVPVTPSz96AK2DOxxS7ahPUNx7PcNwk4o",
+  authDomain: "ptv1-d75e9.firebaseapp.com",
+  databaseURL: "https://ptv1-d75e9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ptv1-d75e9",
+  storageBucket: "ptv1-d75e9.appspot.com",
+  messagingSenderId: "787477365147",
+  appId: "1:787477365147:web:12928bd7cd088415b8a13e"
+};
